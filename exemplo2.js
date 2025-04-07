4becbabc24220c0ede7a1a0596b937e8b9872c30
wns-208 <lordwillias@gmail.com>
2025-04-07 16:26:07 -0300

    s

---
+++ exemplo2.js
@@ -0,0 +1,13 @@
+function somar(n1,n2){
+    var resultado = n1+n2;
+    return resultado;
+}
+
+function subtrair(n1,n2){
+    var resultado = n1-n2;
+    return resultado;
+}
+function dobro(n1){
+    var resultado = n1*2;
+    return resultado;
+}
