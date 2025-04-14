554aab98eb01df8e18fccaf81f9277e77e2b8e70
wns-208 <lordwillias@gmail.com>
2025-04-14 14:50:35 -0300

    w

---
+++ aula_7/exemplo.js
@@ -0,0 +1,17 @@
+function somar(a,b){
+return Number(a+b);
+}
+function subtrair(a,b){
+    return Number(a-b);
+}
+function desconto(a,b){
+    return   a*(b/100);
+    
+}
+function validar(usuarioP,senhaP,usuarioI,senhaI){
+    if(usuarioI==usuarioP&&senhaI==senhaP){
+        return true;
+    } else{
+        return false;
+    }
+}
